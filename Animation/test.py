997269417b714ed4df204f29763d6d11095ddba3
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-01-02 19:35:53 +0700

    edit test

--- Animation/test.py
+++ Animation/test.py
@@ -1,6 +1,18 @@
 from manim import *
-class CreateCircle(Scene):
+class Example(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        orange_square = Square(color=ORANGE, fill_opacity=0.5)
+        blue_circle = Circle(color=BLUE, fill_opacity=0.5)
+        self.add(orange_square)
+        self.play(ReplacementTransform(orange_square, blue_circle, run_time=3))
+        small_dot = Dot()
+        small_dot.add_updater(lambda mob: mob.next_to(blue_circle, DOWN))
+        self.play(Create(small_dot))
+        self.play(blue_circle.animate.shift(LEFT))
+        self.wait()
+        self.play(FadeOut(blue_circle, small_dot))
+        
+        
+with tempconfig({"quality": "medium_quality", "preview": False}):
+    scene = Example()
+    scene.render()
