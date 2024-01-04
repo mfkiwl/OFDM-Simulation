763a89838371fe4bdd72ec41605eb2c06df35bcb
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-01-04 15:36:26 +0700

    OFDM package

---
+++ Source/OFDM/channel.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import scipy.fftpack
+import math as math
+# from .helper import PS_FIXED
+
+
+def group_bits(bits, bits_per_group):
+    bits_groups = []
+    x = 0
+    num_groups = (len(bits)//bits_per_group)+1
+    for i in range(num_groups):
+        bits_groups.append(bits[x:x+bits_per_group])
+        x = x+bits_per_group
+    bits_groups[-1] = np.zeros(bits_per_group, dtype = int)
+    return bits_groups
+
+
+def group_bits_zero_pad(bits, bits_per_group):
+    num_groups = (len(bits) + bits_per_group - 1) // bits_per_group
+    # Pad the bits list with zeros if needed
+    bits_zero_pad = bits
+    bits_zero_pad.resize(num_groups*bits_per_group, refcheck=False)
+    # Use a list comprehension to create the groups
+    bits_groups = [bits_zero_pad[i:i + bits_per_group] for i in range(0, len(bits), bits_per_group)]
+    return bits_groups
+
+
+def S2P(bits_serial : np.ndarray, length : int, mu : int):
+    return bits_serial.reshape(length, mu)
+
+
+def P2S(bits_parallel : np.ndarray):
+    return bits_parallel.reshape((-1,))
+
+
+def Mapping(bits, mapping_table):
+    return np.array([mapping_table[tuple(b)] for b in bits])
+
+
+def Demapping(QAM, demapping_table):
+    # array of possible constellation points
+    constellation = np.array([x for x in demapping_table.keys()])
+    # calculate distance of each RX point to each possible point
+    dists = abs(QAM.reshape((-1,1)) - constellation.reshape((1,-1)))
+    # for each element in QAM, choose the index in constellation
+    # that belongs to the nearest constellation point
+    const_index = dists.argmin(axis=1)
+    # get back the real constellation point
+    hardDecision = constellation[const_index]
+    # transform the constellation point into the bit groups
+    return np.vstack([demapping_table[C] for C in hardDecision]), hardDecision
+
+
+def OFDM_symbol(QAM_payload, K, dataCarriers, pilotCarriers, pilotValue):
+    symbol = np.zeros(K, dtype=complex) # the overall K subcarriers
+    symbol[pilotCarriers] = pilotValue  # allocate the pilot subcarriers
+    symbol[dataCarriers] = QAM_payload  # allocate the pilot subcarriers
+    return symbol
+
+
+def FFT(OFDM_RX):
+    return np.fft.fft(OFDM_RX)
+
+
+def IFFT(OFDM_data):
+    return np.fft.ifft(OFDM_data)
+
+
+def addCP(OFDM_time, CP):
+    cp = OFDM_time[-CP:]               # take the last CP samples ...
+    return np.hstack([cp, OFDM_time])  # add them to the beginning
+
+
+def removeCP(signal, CP, K):
+    return signal[CP:(CP+K)]
+
+
+def equalize(OFDM_demod, Hest):
+    return OFDM_demod / Hest
+
+
+def get_payload(equalized, dataCarriers):
+    return equalized[dataCarriers]
+
+
+def channel(signal, channelResponse, snrdb):
+    convolved = np.convolve(signal, channelResponse)
+    signal_power = np.mean(abs(convolved**2))
+    sigma2 = signal_power * 10**(-snrdb/10) # calculate noise power based on signal power and SNR
+    # print ("RX Signal power: %.4f. Noise power: %.4f" % (signal_power, sigma2))
+    # Generate complex noise with given variance
+    noise = np.sqrt(sigma2/2) * (np.random.randn(*convolved.shape)+1j*np.random.randn(*convolved.shape))
+    return convolved + noise
+
+
+def channelEstimate(OFDM_demod, allCarriers, pilotCarriers, pilotValue):
+    '''
+    Perform interpolation between the pilot carriers to get an estimate
+    of the channel in the data carriers. Here, we interpolate absolute value and phase
+    separately
+    '''
+    pilots = OFDM_demod[pilotCarriers]      # extract the pilot values from the RX signal
+    Hest_at_pilots = pilots / pilotValue    # divide by the transmitted pilot values
+    Hest_abs = scipy.interpolate.interp1d(pilotCarriers, abs(Hest_at_pilots), kind='linear')(allCarriers)
+    Hest_phase = scipy.interpolate.interp1d(pilotCarriers, np.angle(Hest_at_pilots), kind='linear')(allCarriers)
+    Hest = Hest_abs * np.exp(1j*Hest_phase)
+    return Hest, Hest_at_pilots
---
+++ Source/OFDM/helper.py
@@ -0,0 +1,109 @@
+import numpy as np
+import cv2 as cv
+import requests
+
+
+def xor(a, b):
+    # initialize result
+    result = []
+    # same, then XOR is 0, else 1
+    for i in range(1, len(b)):
+        if a[i] == b[i]:
+            result.append('0')
+        else:
+            result.append('1')
+    return ''.join(result)
+
+
+def url2img(urls, RGB = False):
+    resp = requests.get(urls, stream=True).raw
+    img = np.asarray(bytearray(resp.read()), dtype="uint8")
+    img = cv.imdecode(img, cv.IMREAD_COLOR)
+    if RGB :
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    return img
+
+
+def mod2div(divident, divisor):
+    # Number of bits to be XORed at a time.
+    pick = len(divisor)
+    # Slicing the divident to appropriate
+    # length for particular step
+    tmp = divident[0 : pick]
+    while pick < len(divident):
+        if tmp[0] == '1':
+            # replace the divident by the result
+            # of XOR and pull 1 bit down
+            tmp = xor(divisor, tmp) + divident[pick]
+        else:
+            # If the leftmost bit of the dividend (or the
+            # part used in each step) is 0, the step cannot
+            # use the regular divisor; we need to use an
+            # all-0s divisor.
+            tmp = xor('0'*pick, tmp) + divident[pick]
+
+        # increment pick to move further
+        pick += 1
+
+    # For the last n bits, we have to carry it out
+    # normally as increased value of pick will cause
+    # Index Out of Bounds.
+    if tmp[0] == '1':
+        tmp = xor(divisor, tmp)
+    else:
+        tmp = xor('0'*pick, tmp)
+
+    checkword = tmp
+    return checkword
+
+
+def encodeData(data, key):
+    l_key = len(key)
+    # Appends n-1 zeroes at end of data
+    appended_data = data + '0'*(l_key-1)
+    remainder = mod2div(appended_data, key)
+    # Append remainder in the original data
+    codeword = data + remainder
+    return codeword
+
+
+def decodeData(data, key):
+    l_key = len(key)
+    # Appends n-1 zeroes at end of data
+    appended_data = data + '0'*(l_key-1)
+    remainder = mod2div(appended_data, key)
+    return remainder
+
+
+def count_error(data):
+    count = 0
+    for i in data:
+        if i != '000':
+            if i!= '00':
+                count += 1
+    return count # number error bits
+
+
+def split(word):
+    return [char for char in word]
+
+
+def resize_image(img_src, max_size):
+    height, width = img_src.shape[:2] # Get the original image dimensions
+    # Calculate the new dimensions while maintaining the aspect ratio
+    if width > height:
+        new_width = max_size
+        new_height = int(max_size * (height / width))
+    else:
+        new_height = max_size
+        new_width = int(max_size * (width / height))
+
+    return cv.resize(img_src, (new_width, new_height), cv.INTER_LINEAR)
+
+
+def rescale_image(img_src, scale):
+    height, width = img_src.shape[:2] # Get the original image dimensions
+    # Calculate the new dimensions based on the scale factor
+    new_width = int(width * scale)
+    new_height = int(height * scale)
+    return cv.resize(img_src, (new_width, new_height), cv.INTER_LINEAR)
