9a65c30e5a0de704cda57d0828d0137d4fac3e7f
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-01-05 18:46:44 +0700

    move crc related funtions to another files

---
+++ Source/OFDM/crc.py
@@ -0,0 +1,70 @@
+def xor(a, b):
+    # initialize result
+    result = []
+    # same, then XOR is 0, else 1
+    for i in range(1, len(b)):
+        if a[i] == b[i]:
+            result.append('0')
+        else:
+            result.append('1')
+    return ''.join(result)
+
+
+def mod2div(divident, divisor):
+    # Number of bits to be XORed at a time.
+    pick = len(divisor)
+    # Slicing the divident to appropriate
+    # length for particular step
+    tmp = divident[0 : pick]
+    while pick < len(divident):
+        if tmp[0] == '1':
+            # replace the divident by the result
+            # of XOR and pull 1 bit down
+            tmp = xor(divisor, tmp) + divident[pick]
+        else:
+            # If the leftmost bit of the dividend (or the
+            # part used in each step) is 0, the step cannot
+            # use the regular divisor; we need to use an
+            # all-0s divisor.
+            tmp = xor('0'*pick, tmp) + divident[pick]
+
+        # increment pick to move further
+        pick += 1
+
+    # For the last n bits, we have to carry it out
+    # normally as increased value of pick will cause
+    # Index Out of Bounds.
+    if tmp[0] == '1':
+        tmp = xor(divisor, tmp)
+    else:
+        tmp = xor('0'*pick, tmp)
+
+    checkword = tmp
+    return checkword
+
+
+def encodeData(data, key):
+    l_key = len(key)
+    # Appends n-1 zeroes at end of data
+    appended_data = data + '0'*(l_key-1)
+    remainder = mod2div(appended_data, key)
+    # Append remainder in the original data
+    codeword = data + remainder
+    return codeword
+
+
+def decodeData(data, key):
+    l_key = len(key)
+    # Appends n-1 zeroes at end of data
+    appended_data = data + '0'*(l_key-1)
+    remainder = mod2div(appended_data, key)
+    return remainder
+
+
+def count_error(data):
+    count = 0
+    for i in data:
+        if i != '000':
+            if i!= '00':
+                count += 1
+    return count # number error bits
