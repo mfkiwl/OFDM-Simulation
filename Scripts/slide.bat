7561f594d5fba29793a477805bc7b8bd12a2e094
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-01-09 14:33:32 +0700

    edit scripts

--- Scripts/slide.bat
+++ Scripts/slide.bat
@@ -1,2 +1,2 @@
 @echo off
-jupyter nbconvert --to slides --post serve Source\64_QAM_OFDM.ipynb --SlidesExporter.reveal_scroll=True
+jupyter nbconvert --to slides --post serve Source\OFDM.ipynb --SlidesExporter.reveal_scroll=True
